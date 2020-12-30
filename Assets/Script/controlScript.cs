f62c464e4c41c26b58194e844c70d03225c0d214
nugrahas <adithamezonk@gmail.com>
2020-12-30 12:04:25 +0700

    AR pertemuan 16

---
+++ Assets/Script/controlScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controlScript : MonoBehaviour
+{
+	AudioSource audio;
+	public AudioClip[] clips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        	audio = GetComponent<AudioSource>();
+        	StartCoroutine(introJingle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator introJingle(){
+    	yield return new WaitForSeconds(1);
+    	playSound(0);
+    	StartCoroutine(bunyiBurung());
+    }
+
+    private IEnumerator bunyiBurung(){
+    	yield return new WaitForSeconds(1);
+    	playSound(1);
+    	StartCoroutine(anjingMenyalak());
+    }
+
+    private IEnumerator anjingMenyalak(){
+    	yield return new WaitForSeconds(1);
+    	playSound(2);
+    	StartCoroutine(tembakJingle());
+    }
+
+    private IEnumerator tembakJingle(){
+    	yield return new WaitForSeconds(1);
+    	playSound(3);
+    }
+
+    private void playSound(int sound){
+	 	audio.clip = clips[sound];
+	 	audio.Play();
+ 	}
+
+ 	public void pindahGame(){
+ 		SceneManager.LoadScene("SampleScene");
+ 	}
+}
